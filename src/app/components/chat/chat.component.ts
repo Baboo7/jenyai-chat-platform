98d034ca4e22a049da24e938bfe7ef9eeea9ac95
Baboo7 <baptiste.studer@laposte.net>
2017-10-09 22:43:21 -0700

    feature/scroll conversation when new message in interface

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input } from '@angular/core';
+import { Component, EventEmitter, Input, ElementRef, ViewChild, AfterViewChecked, DoCheck } from '@angular/core';
 
 import { WebsocketService } from '../../services/websocket.service';
 import Utils from '../../utils';
@@ -8,14 +8,36 @@ import Utils from '../../utils';
   templateUrl: 'chat.component.html',
   styleUrls: [ 'chat.component.scss' ]
 })
-export class ChatComponent {
+export class ChatComponent implements AfterViewChecked, DoCheck {
 
+  private totalMsgs: number = 0;
+  private toScroll: boolean;
   private userInput = '';
+
   @Input() private emitterType;
   @Input() private recipient;
   @Input() private messages;
+  @ViewChild('convTainer') private convTainer: ElementRef;
 
   constructor(private websocket: WebsocketService) { }
+
+  ngAfterViewChecked() {
+    if (this.toScroll) {
+      try {
+        this.convTainer.nativeElement.scrollTop = this.convTainer.nativeElement.scrollHeight;
+      } catch(err) { }
+      this.toScroll = false;
+    }
+  }
+
+  ngDoCheck() {
+    let newTotalMsgs = 0;
+    if (this.messages) { newTotalMsgs = this.messages.length; }
+    if (this.totalMsgs !== newTotalMsgs) {
+      this.toScroll = true;
+      this.totalMsgs = newTotalMsgs;
+    }
+  }
 
   private sendMessage(): void {
     if (Utils.isEmpty(this.userInput)) {
