a204c66b1ed0e7cbb5b3c14c8ab4adb179026e32
AnneAi <31939324+AnneAi@users.noreply.github.com>
2017-10-23 11:04:50 -0700

    send contact information from landing page on form validation (#6)

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { environment } from '../../../environments/environment';
 
 @Component ({
   selector: 'landing-page',
@@ -7,12 +10,29 @@ import { Component } from '@angular/core';
 })
 export class LandingPageComponent {
 
-  private contacted: boolean = false;
-
+  private contacted: boolean = false
+  private name: string;
+  private email: string;
+  private phone: string;
+  private msg: string;
   private emailPatrn = /([A-Z]|[a-z]|[0-9]|-|_)+@([A-Z]|[a-z]|[0-9]|-|_)+\.([a-z])/;
   private phonePatrn = /[0-9]/;
 
+  constructor(private http: HttpClient) { }
+
   contact(): void {
-    this.contacted = true;
+    let body = {
+      name: this.name,
+      email: this.email,
+      phone: this.phone,
+      message: this.msg
+    };
+
+    this.http.post(`${environment.api}/contact`, body)
+    .subscribe((data: any) => {
+      if (data.success) {  
+        this.contacted = true;
+      }
+    })
   }
 }
